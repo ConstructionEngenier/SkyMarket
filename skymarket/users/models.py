70e942f6c09f1b254aeecf91f1e1da6f32140edf
Artem <artem.mikhaylov5593@yandex.ru>
2022-10-02 18:14:25 +0300

    Created User's model

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,14 +1,45 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
-from users.managers import UserManager
+from skymarket.users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+class UserRoles(Enum):
+    USER = "user"
+    ADMIN = "admin"
 
 
 class User(AbstractBaseUser):
-    pass
+    # эта константа определяет поле для логина пользователя
+    USERNAME_FIELD = 'email'
+
+    # эта константа содержит список с полями,
+    # которые необходимо заполнить при создании пользователя
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', "role"]
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    objects = UserManager()
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
