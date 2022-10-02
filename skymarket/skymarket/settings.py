7d5f6afc13fc0cbb1b2d6c6f1f6c64fb3f9be69a
Artem <artem.mikhaylov5593@yandex.ru>
2022-10-02 16:51:25 +0300

    Added rest_framework swagger and corsheaders

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-# TODO здесь тоже нужно подключить Swagger и corsheaders
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -42,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework_swagger",
     "rest_framework",
     "users",
     "ads",
@@ -80,8 +81,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "skymarket.wsgi.application"
 
-# TODO здесь мы настраиваем аутентификацию и пагинацию
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 # TODO здесь мы настраиваем Djoser
 DJOSER = {
