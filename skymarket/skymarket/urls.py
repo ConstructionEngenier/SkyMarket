cc08ec8471d053ab8597e43f49994e4b9d475672
Artem <artem.mikhaylov5593@yandex.ru>
2022-10-20 23:47:39 +0300

    Have been filled app urls

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
-    path("", include("users.urls")),
+    path("api/", include("users.urls")),
+    path("api/", include("ads.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
