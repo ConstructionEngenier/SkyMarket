af9a3e4449f7c09a3c8dd98a0699353e0d82789c
Artem <artem.mikhaylov5593@yandex.ru>
2022-10-20 23:53:27 +0300

    ads permissions, filters and urls have been filled

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,20 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return bool(request.user and request.user.is_authenticated)
+        if request.user == obj.author:
+            return True
+
+        return False
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser == True:
+            return True
+        else:
+            return False
--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,8 +1,42 @@
 from django.urls import include, path
+from rest_framework_nested import routers
 
-# TODO настройка роутов для модели
+from ads.views import AdViewSet, CommentViewSet
 
+# urlpatterns = [
+#     path('', views.AdViewSet.as_view({
+#         'get': 'list',
+#         'post': 'create',
+#     })),
+#
+#     path('me/', views.AdViewSet.as_view({'get': 'me'})),
+#     path('<int:pk>/', views.AdViewSet.as_view({
+#         'get': 'retrieve',
+#         'patch': 'partial_update',
+#         'delete': 'destroy'
+#     })),
+#
+#     path('<int:ad>/comments/', views.CommentViewSet.as_view({
+#         'get': 'list',
+#         'post': 'create'
+#     })),
+#
+#     path('<int:ad>/comments/<int:id>/', views.CommentViewSet.as_view({
+#         'get': 'retrieve',
+#         'patch': 'partial_update',
+#         'delete': 'destroy'
+#     })),
+# ]
+
+
+ads_router = routers.SimpleRouter()
+ads_router.register(r"ads", AdViewSet)
+
+ads_router.register("ads", AdViewSet, basename="users")
+comment_router = routers.NestedSimpleRouter(ads_router, r"ads", lookup="ad")
+comment_router.register(r"comments", CommentViewSet, basename="comments")
 
 urlpatterns = [
-
+    path("", include(ads_router.urls)),
+    path("", include(comment_router.urls)),
 ]
